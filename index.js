a5b0130a1c5e56060089e17895291e7fca3afa93
jms90 <56556997+jms90@users.noreply.github.com>
2021-02-25 17:31:47 +0100

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(express.json({ extended: true }))
 
 //Puerto de la app
-const PORT = process.env.PORT || 4000;
+const port = process.env.port || 4000;
 
 //Importar rutas
 app.use('/api/usuarios', require('./routes/usuarios'));
@@ -22,6 +22,6 @@ app.use('/api/auth', require('./routes/auth'));
 app.use('/api/proyectos', require('./routes/proyectos'));
 app.use('/api/tareas', require('./routes/tareas'));
 //run ap
-app.listen(PORT, () => {
+app.listen(port,'0.0.0.0', () => {
     console.log(`El servidor esta funcionando en en puerto ${PORT}`)
 })
